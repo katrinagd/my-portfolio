689bc0c6998cc47ecb04f82e190ed05cf103137e
Katrina Guyduy <katrinaguyduy@gmail.com>
2016-02-23 09:37:02 -0800

    Added Image Overlay Effect

--- js/script.js
+++ js/script.js
@@ -25,4 +25,44 @@ $(document).ready(function(){
 		// Stop link behaviour
 		return false;
 	});
+
+    // Mouseenter overlay
+	$('ul#gallery li').on('mouseenter', function(){
+		// Get data attribute value
+		var title = $(this).children().data('title');
+		var desc = $(this).children().data('desc');
+
+		// Validation
+		if(desc == null){
+			desc ='Click To Enlarge';
+		}
+
+		if(title == null){
+			title = '';
+		}
+
+		// Create overlay div
+        $(this).append('<div class="overlay"></div>');	
+        
+        // Get the overlay div
+        var overlay = $(this).children('.overlay');
+
+        // Add html to overlay
+        overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');	
+
+        // Fade in overlay
+
+        overlay.fadeIn(800);
+	});
+	// Mouseleave overlay
+	$('ul#gallery li').on('mouseleave', function(){
+		// Create overlay div
+        $(this).append('<div class="overlay"></div>');	
+        
+        // Get the overlay div
+        var overlay = $(this).children('.overlay');
+
+        //Fade out overlay
+        overlay.fadeOut(500);
+	});
 });
